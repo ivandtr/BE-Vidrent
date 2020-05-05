bc0c303a363b5436f27cbb332d888897e94a14c6
idtr <ivanlozmar@gmail.com>
2020-05-05 09:47:02 +0200

    Implemented authentication

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
@@ -14,15 +16,18 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('User already registered.');
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send('User already registered.');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send(_.pick(user, ['_id', 'name', 'email']));
+  const token = user.generateAuthToken();
+  res
+    .header('x-auth-token', token)
+    .send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 // router.put('/:id', async (req, res) => {
