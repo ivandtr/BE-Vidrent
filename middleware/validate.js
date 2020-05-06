d2a1a25ca96a935895a3362e25f54cfcd330c3d3
idtr <ivanlozmar@gmail.com>
2020-05-06 10:40:50 +0200

    Added returns routes

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,6 +1,7 @@
 module.exports = (validator) => {
   return (req, res, next) => {
     const { error } = validator(req.body);
+    const i = 0;
     if (error) return res.status(400).send(error.details[0].message);
     next();
   };
